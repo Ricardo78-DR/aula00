4d5754b145cfaa35023817a563d859480981cfc8
Ricardo Silva <ricardoss159@gmail.com>
2023-11-26 22:02:23 -0300

    primeiro commit

---
+++ aula00.py
@@ -0,0 +1,18 @@
+
+
+# Contagem acabou no 12, pois a condição do if é que quando numero for igual a 13 o laço seja finalizado.
+for i in range (1, 20):
+    if(i == 13):
+        continue
+    else:
+        print(i)
+
+
+# Condicional WHILE, decrementando os Andares.
+for i in range(20, 0, -1):
+    if( i == 13):
+         continue
+    else:
+        print("Andar", i)
+    
+
